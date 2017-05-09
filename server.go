6a3f630817012a664cf9d105b2c8ab34e46b128c
Shannor Trotty <shannor.a.trotty@gmail.com>
2017-05-09 13:19:28 -0400

    removed code for website

--- server.go
+++ server.go
@@ -1,16 +1,14 @@
 package main
 
+/**
+	Server is the middle man between web/mobile to database
+*/
 import (
 	"net/http"
 )
 
 
 func main() {
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintf(w, "Welcome to Grunt Comics!")
-	// 	return
-	// })
-	http.Handle("/", http.FileServer(http.Dir("./static")))
 
 	http.ListenAndServe(":8080", nil)
 
