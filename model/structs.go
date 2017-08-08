4ded52f4d98c419774e057b5ce27ab21b4dd5806
Shannor Trotty <shannor.a.trotty@gmail.com>
2017-08-08 01:30:52 -0400

    made changes to the format of everything. Changing to make use of interfaces

--- model/structs.go
+++ model/structs.go
@@ -3,13 +3,56 @@ package model
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/julienschmidt/httprouter"
 )
+
+type GrunttAPI interface {
+	GetAllComics() httprouter.Handle
+	GetPopularComics() httprouter.Handle
+	GetComicDescription() httprouter.Handle
+	GetComicChapterList() httprouter.Handle
+	GetChapterPages() httprouter.Handle
+	GetSearchCategories() httprouter.Handle
+	Search() httprouter.Handle
+}
+
+type api struct {
+	ComicExtra Webcrawler
+	ReadComics Webcrawler
+}
+
+type Webcrawler interface {
+	CreateAllComicsURL() string
+	CreatePopularComicsURL(int) (string, error)
+	CreateComicChapterListURL(string) (string, error)
+	CreateChapterPagesURL(string, int) (string, error)
+	CreateComicDescriptionURL(string) (string, error)
+	CreateSearchURL() (string, error)
+	GetAllComics(*goquery.Document) (Comics, error)
+	GetPopularComics(*goquery.Document) (PopularComics, error)
+	GetComicChapterList(*goquery.Document, string) (Chapters, error)
+	GetChapterPages(*goquery.Document, string, int) ([]string, error)
+	GetSearchCategories() httprouter.Handle
+	GetComicDescription() httprouter.Handle
+	Search(*goquery.Document) httprouter.Handle
+}
+
+type ComicExtra struct {
+	BaseURL string
+}
+
+type ReadComics struct {
+	BaseURL string
+}
 
 type Comic struct {
 	Title    string `json:"title"`
 	Link     string `json:"link"`
 	Category string `json:"category"`
 }
+type Comics []Comic
 
 type PopularComic struct {
 	Title      string `json:"title"`
@@ -17,12 +60,22 @@ type PopularComic struct {
 	Img        string `json:"img"`
 	IssueCount int    `json:"issueCount"`
 }
+type PopularComics []PopularComic
 
 type Chapter struct {
 	ChapterName string `json:"chapterName"`
 	Link        string `json:"link"`
 	ReleaseDate string `json:"releaseDate"`
 }
+type Chapters []Chapter
+
+type SearchResult struct {
+	Title string `json:"title"`
+	Link  string `json:"link"`
+	Img   string `json:"img"`
+	Genre string `json:"genre"`
+}
+type SearchResults []SearchResult
 
 // RequestError  error for when the provided param doesn't match
 type RequestError struct {
